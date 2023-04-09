41887809749f34850efdf21beca3b6250bed0cba
McMek <ercanselim@gmail.com>
2023-04-09 22:09:42 +0200

    added optional and nonoptional argument check using "=" as a trigger in between strings eg. "string1=string2"

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include <stdio.h>
 using namespace std;
 
 int main(int argc, char* argv[])
@@ -41,7 +43,7 @@ int main(int argc, char* argv[])
 			exit(1);
 		}
 
-	cout << "\n";
+	cout << endl;
 
 	ifstream testtxt;
 
@@ -49,15 +51,61 @@ int main(int argc, char* argv[])
 
 	if (testtxt)
 	{
-		cout << "test.txt detected\n";
+		cout << "test.txt detected\n\n";
 	}
 	else
 	{
-		cout << "test.txt not detected\n";
+		cout << "test.txt not detected\n\n";
 	}
 
-	cout << "\n";
+	//adding string check for "=" sign
 
+	for (int i = 1; i < argc; i++)
+	{
+		string argi = argv[i];
+
+		
+
+		if (argi.find('=') != string::npos)
+			{
+				ofstream writearg;
+				writearg.open("test1.txt");
+				writearg << argv[i];
+				writearg.close();
+				
+				ifstream in("test1.txt");
+				stringstream buffer;
+				buffer << in.rdbuf();
+				string test = buffer.str();
+				cout << "Equal sign found at "<< test << " argument." << endl << endl;
+
+				size_t pos1 = 0;
+				size_t pos2;
+
+				string str;
+				string str2;
+				pos2 = test.find("=", pos1); 
+
+				str = test.substr(pos1, (pos2 - pos1)); 
+				cout << "Argument: \"" << str << "\" initiated with its: ";
+
+				pos2 = pos2 + 1;
+				str2 = test.substr(pos2, (pos1 - pos2));
+				cout << "\"" << str2 << "\" optional argument\n" << endl;
+				
+			} 
+		
+	}
+
+	const int result = remove("test1.txt");
+	/*
+	if (result == 0) {
+		cout << "success\n";
+	}
+	else {
+		cout << "unsuccsessfull";
+	}
+	*/
 	return 0;
 
 }
