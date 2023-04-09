f1cb145ea4bdd6e22f1647f9c9e8dd3ebec020a1
McMek <ercanselim@gmail.com>
2023-04-10 00:30:42 +0200

    organized everything to ArgumentParser function

--- main.cpp
+++ main.cpp
@@ -6,51 +6,58 @@
 #include <filesystem>
 using namespace std;
 
+void ArgumentParser(int argc, char* argv[]);
+
 int main(int argc, char* argv[])
 {
-		string match;
+		
+	ArgumentParser(argc, argv);
 
-		for (int i = 1; i < argc; i++)
-		{
-			string argi = argv[i];
+	return 0;
 
-			if (argi.find("test") == 0)
-				match = argi.substr(0, 4);
-		}
+}
 
-		bool found = false;
+void ArgumentParser(int argc, char* argv[])
+{
+	string match;
 
-		for (int i = 1; i < argc; i++)
-		{
-			string argi = argv[i];
+	for (int i = 1; i < argc; i++)
+	{
+		string argi = argv[i];
 
-			if (argi.find("test") == 0)
-				if (argi == match)
-					found = true;
-		}
+		if (argi.find("test") == 0)
+			match = argi.substr(0, 4);
+	}
 
-		if (found)
-		{
-			cout << "\n\"Test\" argument initiated" << endl;
+	bool found = false;
 
-			int count;
-			cout << "\nCommand-line arguments:\n";
-			for (count = 1; count < argc; count++)
-				cout << "  argv[" << count << "]   " << argv[count] << "\n";
-		}
-		else
-		{
-			cout << "\nUsage: appwithParameters.exe test\n" << endl;
-			exit(1);
-		}
+	for (int i = 1; i < argc; i++)
+	{
+		string argi = argv[i];
+
+		if (argi.find("test") == 0)
+			if (argi == match)
+				found = true;
+	}
+
+	if (found)
+	{
+		cout << "\n\"Test\" argument initiated" << endl;
+
+		int count;
+		cout << "\nCommand-line arguments:\n";
+		for (count = 1; count < argc; count++)
+			cout << "  argv[" << count << "]   " << argv[count] << "\n";
+	}
+	else
+	{
+		cout << "\nUsage: appwithParameters.exe test\n" << endl;
+		exit(1);
+	}
 
 	cout << endl;
 
-	//ifstream testtxt;
-
-	//testtxt.open("test.txt");
-
-	if (std::filesystem::exists("test.txt"))
+	if (filesystem::exists("test.txt"))
 	{
 		cout << "test.txt detected\n\n";
 	}
@@ -65,48 +72,43 @@ int main(int argc, char* argv[])
 	{
 		string argi = argv[i];
 
-		
-
 		if (argi.find('=') != string::npos)
-			{
-				ofstream writearg;
-				writearg.open("test1.txt");
-				writearg << argv[i];
-				writearg.close();
-				
-				ifstream in("test1.txt");
-				stringstream buffer;
-				buffer << in.rdbuf();
-				string test = buffer.str();
-				cout << "Equal sign found at "<< test << " argument." << endl << endl;
+		{
+			ofstream writearg;
+			writearg.open("test1.txt");
+			writearg << argv[i];
+			writearg.close();
 
-				size_t pos1 = 0;
-				size_t pos2;
+			ifstream in("test1.txt");
+			stringstream buffer;
+			buffer << in.rdbuf();
+			string test = buffer.str();
+			cout << "Equal sign found at " << test << " argument." << endl << endl;
 
-				string str;
-				string str2;
-				pos2 = test.find("=", pos1); 
+			size_t pos1 = 0;
+			size_t pos2;
 
-				str = test.substr(pos1, (pos2 - pos1)); 
-				cout << "Argument: \"" << str << "\" initiated with its: ";
+			string str;
+			string str2;
+			pos2 = test.find("=", pos1);
 
-				pos2 = pos2 + 1;
-				str2 = test.substr(pos2, (pos1 - pos2));
-				cout << "\"" << str2 << "\" optional argument\n" << endl;
-				
-			} 
-		
+			str = test.substr(pos1, (pos2 - pos1));
+			cout << "Argument: \"" << str << "\" initiated with its: ";
+
+			pos2 = pos2 + 1;
+			str2 = test.substr(pos2, (pos1 - pos2));
+			cout << "\"" << str2 << "\" optional argument\n" << endl;
+
+		}
+
+		const int result = remove("test1.txt");
+		/*
+		if (result == 0) {
+			cout << "success\n";
+		}
+		else {
+			cout << "unsuccsessfull";
+		}*/
+
 	}
-
-	const int result = remove("test1.txt");
-	/*
-	if (result == 0) {
-		cout << "success\n";
-	}
-	else {
-		cout << "unsuccsessfull";
-	}
-	*/
-	return 0;
-
 }
