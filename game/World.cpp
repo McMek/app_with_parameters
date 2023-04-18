3439fa9a2ef54b1cd87cdf9e22b70df2a644b83e
McMek <ercanselim@gmail.com>
2023-04-19 01:34:22 +0200

    minor changes

--- game/World.cpp
+++ game/World.cpp
@@ -2,4 +2,4 @@
 
 World::World() : m_size(100), m_worldmap(10000, 0) {}
 
-World::World(int size) : m_size(size), m_worldmap(size* size, 0) {}
+World::World(int size) : m_size(size), m_worldmap(size * size, 0) {}
