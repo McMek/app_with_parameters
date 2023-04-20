be4acedb9ac4d1ae46bc62bcc696a4e5d6a394b9
Taner Selim <taner.selim@gameforge.com>
2023-04-20 15:49:58 +0200

    Actually draw something on the frame buffer

--- graphics/graphicstest.cpp
+++ graphics/graphicstest.cpp
@@ -1,12 +1,16 @@
 // Just a testing method to make sure we have included everything properly
 #include "graphicstest.h"
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 namespace TestGraphics
 {
 	void DrawTestWindow()
 	{
-		sf::Window window(sf::VideoMode(800, 600), "My window");
+		sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+		
+		window.setFramerateLimit(60); // set the framerate
+
+
 
 		// run the program as long as the window is open
 		while (window.isOpen())
@@ -19,6 +23,24 @@ namespace TestGraphics
 				if (event.type == sf::Event::Closed)
 					window.close();
 			}
+
+			// clear the window with black color
+			window.clear(sf::Color::Black);
+
+			// draw everything here...
+			// window.draw(...);
+
+			sf::CircleShape shape(50.f);
+
+			// set the shape color to green
+			shape.setFillColor(sf::Color(100, 250, 50));
+
+			window.draw(shape);
+
+
+			// end the current frame
+			window.display();
 		}
+
 	}
 }
