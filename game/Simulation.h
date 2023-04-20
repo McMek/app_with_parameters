c6ee9c4f26e034fbf7652468fd5ac31585ebf964
McMek <ercanselim@gmail.com>
2023-04-20 16:58:02 +0200

    simulation.h updated

--- game/Simulation.h
+++ game/Simulation.h
@@ -1,2 +1,18 @@
 #pragma once
 
+#include "World.h"
+
+class Simulation
+{
+private:
+	const int m_randomSeed = 41;
+	World* m_world;
+	std::vector<Player*> m_players;
+	long m_maxSteps = 5000;
+	long m_currentStep = 0;
+
+	Player* CreatePlayer(int id);
+	World* CreateWorld(int worldSize);
+
+	void PopulateWorld(int numberOfPlayers);
+};
